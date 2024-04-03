130905e9c2bf2593a4e7d457613ac9840472e820
Vandana Singh <vandana@zoopindia.com>
2024-04-04 00:40:16 +0530

    basics react

---
+++ App.js
@@ -0,0 +1,30 @@
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag")
+    ]),
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag")
+    ])
+])
+
+
+    // <div id = "parent">
+    //     <div id="child">
+    //         <h1>I'm h1 tag</h1>
+    //         <h2>I'm h2 tag</h2>
+    //     </div>
+    //     <div id="child2">
+    //         <h1>I'm h1 tag</h1>
+    //         <h2>I'm h2 tag</h2>
+    //     </div>
+    //     </div>
+
+
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello world from react");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
